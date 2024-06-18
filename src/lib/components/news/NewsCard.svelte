78508b6d592880c91bf47118cff89517f3d0e9b6
Liam Lyness <liamlyness97@gmail.com>
2024-06-18 21:06:48 +0100

    News card component created

---
+++ src/lib/components/news/NewsCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let { title, category, featuredImg }: { title: string; category: string; featuredImg: string } =
+		$props();
+</script>
+
+<div class="group relative h-[350px] hover:cursor-pointer">
+	<img class="h-full w-full object-cover" src={featuredImg} alt="" />
+	<div
+		class="absolute left-0 top-0 h-full w-full overflow-hidden bg-black bg-opacity-40 duration-700 group-hover:bg-opacity-0"
+	>
+		<div
+			class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3 duration-700 ease-in-out group-hover:-left-full"
+		>
+			<p class="text-2xl font-medium text-white">{title}</p>
+		</div>
+		<div
+			class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl delay-75 duration-1000 ease-in-out group-hover:-left-full"
+		>
+			<p class=" text-claret text-sm font-bold">{category}</p>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import NewsCard from '$lib/components/news/NewsCard.svelte';
+
 	let { data } = $props();
 </script>
 
@@ -7,187 +9,21 @@
 		<h1 class="text-avBlue text-4xl font-extrabold uppercase tracking-widest">Explore Content</h1>
 	</div>
 	<div class="grid w-full grid-cols-4">
-		<div class="group relative h-[350px] hover:cursor-pointer">
-			<img class="h-full w-full object-cover" src="/news4.jpg" alt="" />
-			<div
-				class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40 duration-700 group-hover:bg-opacity-0"
-			>
-				<div
-					class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3 duration-700 ease-in-out group-hover:-left-full"
-				>
-					<p class="text-2xl font-medium text-white">Test Post Title</p>
-				</div>
-				<div
-					class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl delay-75 duration-1000 ease-in-out group-hover:-left-full"
-				>
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news5.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 2</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news6.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 3</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news7.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 2</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">Throwback</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news8.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 2</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">Throwback</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news6.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 3</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news5.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 2</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news4.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news4.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news5.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 2</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news6.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 3</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news7.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 2</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">Throwback</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news8.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 2</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">Throwback</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news6.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 3</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news5.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title 2</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
-		<div class="relative h-[350px]">
-			<img class="h-full w-full object-cover" src="/news4.jpg" alt="" />
-			<div class="absolute left-0 top-0 h-full w-full bg-black bg-opacity-40">
-				<div class="bg-claret absolute bottom-9 left-0 z-20 px-8 py-3">
-					<p class="text-2xl font-medium text-white">Test Post Title</p>
-				</div>
-				<div class="bg-avBlue absolute bottom-20 left-0 z-30 px-8 py-2 shadow-xl">
-					<p class=" text-claret text-sm font-bold">News</p>
-				</div>
-			</div>
-		</div>
+		<NewsCard title="Test Post Title" category="News" featuredImg="/news4.jpg" />
+		<NewsCard title="Test Post Title 2" category="Recap" featuredImg="/news5.jpg" />
+		<NewsCard title="Test Post Title 3" category="News" featuredImg="/news6.jpg" />
+		<NewsCard title="Test Post Title 4" category="Throwback" featuredImg="/news7.jpg" />
+		<NewsCard title="Test Post Title 5" category="Throwback" featuredImg="/news8.jpg" />
+		<NewsCard title="Test Post Title 4" category="Throwback" featuredImg="/news7.jpg" />
+		<NewsCard title="Test Post Title 2" category="Recap" featuredImg="/news5.jpg" />
+		<NewsCard title="Test Post Title" category="News" featuredImg="/news4.jpg" />
+		<NewsCard title="Test Post Title" category="News" featuredImg="/news4.jpg" />
+		<NewsCard title="Test Post Title 2" category="Recap" featuredImg="/news5.jpg" />
+		<NewsCard title="Test Post Title 3" category="News" featuredImg="/news6.jpg" />
+		<NewsCard title="Test Post Title 4" category="Throwback" featuredImg="/news7.jpg" />
+		<NewsCard title="Test Post Title 5" category="Throwback" featuredImg="/news8.jpg" />
+		<NewsCard title="Test Post Title 4" category="Throwback" featuredImg="/news7.jpg" />
+		<NewsCard title="Test Post Title 2" category="Recap" featuredImg="/news5.jpg" />
+		<NewsCard title="Test Post Title" category="News" featuredImg="/news4.jpg" />
 	</div>
 </div>
